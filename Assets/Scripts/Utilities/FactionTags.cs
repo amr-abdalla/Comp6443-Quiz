b4aac7a56378db354bc58c6e695874819488984d
amr-abdalla <amr_alaa2@aucegypt.edu>
2026-03-13 10:01:14 -0400

    Added Faction Tags

---
+++ Assets/Scripts/Utilities/FactionTags.cs
@@ -0,0 +1,8 @@
+public static class FactionTags
+{
+	public const string _Rock = "Rock";
+	public const string _Paper = "Paper";
+	public const string _Scissor = "Scissor";
+	public const string _Lizard = "Lizard";
+	public const string _Spock = "Spock";
+}
